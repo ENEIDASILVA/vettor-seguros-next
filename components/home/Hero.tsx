087d6a101ed7a436f7e432dab85e184014511883
ENEIDASILVA <eneida.corretoradeseguros@gmail.com>
2026-07-12 13:33:20 -0300

    Desativa otimizacao do banner

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -2,15 +2,19 @@ import Image from "next/image";
 
 export default function Hero() {
   return (
-    <section id="inicio" className="relative w-full overflow-hidden bg-[#061B3A]">
+    <section
+      id="inicio"
+      className="relative w-full overflow-hidden bg-[#061B3A]"
+    >
       <Image
         src="/Images/hero/hero-vettor.png"
         alt="Vettor Seguros - Proteção inteligente"
         width={1920}
         height={1080}
         priority
-        quality={100}
-        className="h-auto w-full"
+        unoptimized
+        sizes="100vw"
+        className="block h-auto w-full"
       />
     </section>
   );
